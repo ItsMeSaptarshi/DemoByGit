6d8411c4d2ae612bf961128af8a666e48610630d
ItsMeSaptarshi <167565255+ItsMeSaptarshi@users.noreply.github.com>
2024-04-19 22:56:20 +0530

    Create TrigCalc.py

---
+++ TrigCalc.py
@@ -0,0 +1,16 @@
+import math
+
+def trig_values(degrees):
+    radians = math.radians(degrees)
+    sine = math.sin(radians)
+    cosine = math.cos(radians)
+    tangent = math.tan(radians)
+    return sine, cosine, tangent
+
+angle_degrees = float(input("Enter angle in degrees: "))
+
+sine, cosine, tangent = trig_values(angle_degrees)
+
+print("Sine:", sine)
+print("Cosine:", cosine)
+print("Tangent:", tangent)
